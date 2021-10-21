3167b609e04ae715d0b89dbf0653e034e282ad00
Caio Dunkl <caio.cordeiro@deal.com.br>
2021-10-21 16:28:27 -0300

    Add project files.

---
+++ Livraria.Domain/Interfaces/Repositories/ILivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using Livraria.Domain.Entidades;
+using Livraria.Domain.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria.Domain.Interfaces.Repositories
+{
+    public interface ILivroRepository
+    {
+        long Inserir(Livro livro);
+        void Atualizar(Livro livro);
+        void Excluir(long id);
+        List<LivroQueryResult> Listar();
+        LivroQueryResult Obter(long id);
+        bool CheckId(long id);
+    }
+}
